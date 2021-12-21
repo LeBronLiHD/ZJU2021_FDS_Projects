59ca2061e06404ad867b937beef45b98ce670292
LeBronLiHD <lebronlihd@outlook.com>
2021-12-21 22:29:26 +0800

    update main of pro3

--- 3_Project_Hard/main.c
+++ 3_Project_Hard/main.c
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-12-16 17:03:05
- * @LastEditTime: 2021-12-21 20:57:42
+ * @LastEditTime: 2021-12-21 22:29:08
  * @LastEditors: Please set LastEditors
  * @Description: main file of 3_Project_Hard
  * @FilePath: \3_Project_Hard\main.c
@@ -25,11 +25,10 @@ int main()
 {
     scanf("%d %d", &M, &N);
     G = read_graph(M, N);                       // construct the unidirectional graph
-    show_graph(G, M);
     program_start();                            // set up the timer
-    // search_second(basic_dfs(G, M), 0);          // Depth First Search (DFS) & Pruning
+    search_second(basic_dfs(G, M), 0);          // Depth First Search (DFS) & Pruning
     algorithm_one();                            // calculate the time spent
-    // dijkstra_second(dijkstra(G, M), M);         // Dijkstra & DFS
+    dijkstra_second(dijkstra(G, M), M);         // Dijkstra & DFS
     algorithm_two();                            // calculate the time spent
     dijkstra_cut(dijkstra_one_row(G, M), G, M); // Dijkstra & Edge Cutting
     algorithm_three();                          // calculate the time spent
